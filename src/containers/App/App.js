e85985eb08e2b6a55ecdfdea018e5115d253cf01
Diego Bernal <diego91bernal@gmail.com>
2018-08-27 19:15:32 -0700

    feature: add global reques status handler
    * add error/success message action/reducer
    * add react semantic toast

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,27 @@
 import React from "react";
-import {Segment} from "semantic-ui-react";
+import {SemanticToastContainer, toast} from "react-semantic-toasts";
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
-
 import Routes from "../../components/Routes";
+
 import {getErrorMessage} from "../../reducers/index.reducer";
+import {RESET_ERROR_MESSAGE} from "../../actions/requests.actions";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    handleShowErrorMessage = errorMessage => {
+        const {resetErrorMessage} = this.props;
+        resetErrorMessage();
+
+        setTimeout(() => {
+            toast({title: "Error", description: errorMessage, type: "error", time: 4000}, () => {
+                resetErrorMessage();
+            });
+        }, 0);
+    };
 
     render() {
         console.log("PROPS", this.props);
@@ -17,7 +29,8 @@ class App extends React.Component {
         return (
             <React.Fragment>
                 <Routes />
-                {!!errorMessage && <Segment color="red">{errorMessage}</Segment>}
+                <SemanticToastContainer position="bottom-left" />
+                {!!errorMessage && this.handleShowErrorMessage(errorMessage)}
             </React.Fragment>
         );
     }
@@ -28,4 +41,13 @@ const mapStateToProps = state => ({
     successMessage: state.successMessage
 });
 
-export default withRouter(connect(mapStateToProps)(App));
+const mapDispatchToProps = dispatch => ({
+    resetErrorMessage: () => dispatch(RESET_ERROR_MESSAGE())
+});
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(App)
+);
